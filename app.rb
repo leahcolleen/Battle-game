c6e0115d594316950df3e37c8a73e524720e3667
Leah <leahpitter95@gmail.com>
2021-09-09 15:55:59 +0100

    used the session to store usernames and accessed session from within a route

--- app.rb
+++ app.rb
@@ -1,18 +1,27 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
 
+
 class Battle < Sinatra::Base
   configure :development do
     register Sinatra::Reloader
   end
+
+  enable :sessions
 
   get "/" do
     erb :index
   end
 
   post "/names" do
-    @player_1_name = params[:player_1_name]
-    @player_2_name = params[:player_2_name]
+    session[:player_1_name]= params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
     erb :play
   end
 
